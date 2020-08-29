431771b5ef6f1e829bf313fce3d9bc8a640d9273
KAAN DIKEC <21171681+dikeckaan@users.noreply.github.com>
2020-08-30 01:24:56 +0300

    Create easygsi.sh
    new test script

---
+++ easygsi.sh
@@ -0,0 +1,31 @@
+romurl='https://bigota.d.miui.com/V12.0.3.0.QFJMIXM/miui_DAVINCIGlobal_V12.0.3.0.QFJMIXM_ed720ed0a6_10.0.zip'
+rombase='MIUI'
+zipname=''
+page='index.html'
+uploadto='wet'
+toolkit='https://github.com/erfanoabdi/ErfanGSIs'
+
+echo "Initializing environment"
+sudo -E apt-get -qq update
+sudo -E apt-get -qq install git openjdk-8-jdk wget
+git clone --recurse-submodules "$toolkit" 
+
+echo "Setting up ErfanGSI requirements"
+sudo chmod -R 777 ErfanGSIs
+cd ErfanGSIs
+sudo bash setup.sh
+
+echo "Download Stock Rom & Generate GSI"
+sudo ./url2GSI.sh $romurl $rombase
+
+echo "Zip Aonly and upload"
+sudo chmod -R 777 output
+cd output
+zip -r $ZIP_NAME-GSI-Aonly.7z *-Aonly-*.img
+curl -sL https://git.io/file-transfer | sh
+clear
+./transfer $MIR $ZIP_NAME-GSI-Aonly.7z
+
+echo "Zip AB and upload"
+zip -r $ZIP_NAME-GSI-AB.7z *-AB-*.img
+./transfer $MIR $ZIP_NAME-GSI-AB.7z
