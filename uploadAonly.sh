0e8b60c5bd9a46fdba163090ae0f9e27963ac2ee
KAAN DIKEC <21171681+dikeckaan@users.noreply.github.com>
2020-08-30 02:23:29 +0300

    Update uploadAonly.sh

--- uploadAonly.sh
+++ uploadAonly.sh
@@ -2,5 +2,4 @@ echo "Zip Aonly and upload"
 cd ErfanGSI
 sudo chmod -R 777 output
 cd ErfanGSI/output
-zip -r $devicecodename-GSI-Aonly.7z *-Aonly-*.img
 ./transfer $uploadto $devicecodename-GSI-Aonly.7z
